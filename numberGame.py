928237074458b9f542fb5f1bfba05073e6dea780
Craig Dipuka <craiigdipuka@gmail.com>
2024-06-14 02:30:26 +0200

    first commit

---
+++ numberGame.py
@@ -0,0 +1,9 @@
+import random
+
+print(" Welcome to the Number Guessing Game !\n ")
+
+#Ask User For name 
+
+user_name = input("please enter your name : ")
+user_name = user_name.capitalize()
+print(f"Welcome {user_name}, lets get started :)")
